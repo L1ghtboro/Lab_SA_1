b3a55ae211a991e6d575ff5ed39798b9fa2be8df
L1ghtboro <tiptemka@gmail.com>
2023-09-28 19:47:07 +0200

    Init classes

--- Lab_SA_1/routes/index.js
+++ Lab_SA_1/routes/index.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var router = express.Router();
 
-var { Solution } = require('../public/javascripts/min-max-ant-solution');
+var { MinMaxAntSystem } = require('../public/javascripts/min-max-ant-solution');
 
 /* GET home page. */
 router.get('/', function (req, res) {
