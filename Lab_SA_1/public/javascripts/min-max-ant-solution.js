8b9e55e89ddd900ee57ea5b04be6e1b9bb49ccee
L1ghtboro <tiptemka@gmail.com>
2023-09-28 20:28:54 +0200

    Implement Ant Class Methods for MMAS Algorithm

--- Lab_SA_1/public/javascripts/min-max-ant-solution.js
+++ Lab_SA_1/public/javascripts/min-max-ant-solution.js
@@ -30,15 +30,50 @@ class MinMaxAntSystem {
 
 class Ant {
     constructor(graph, pheromoneMatrix, params) {
-
+        this.graph = graph;
+        this.pheromoneMatrix = pheromoneMatrix;
+        this.params = params;
+        this.visited = new Set();
+        this.tour = [];
+        this.currentNode = params.startNode; // Start node set as default 
+        this.visited.add(params.startNode);
     }
 
+    // This method is used to find the tour (path) for the ant.
     findTour() {
-
+        while (this.visited.size < this.graph.nodes().length) {
+            const nextNode = this.selectNextNode();
+            if (nextNode === -1) break;
+            this.tour.push(nextNode);
+            this.visited.add(nextNode);
+            this.currentNode = nextNode;
+        }
+        return this.tour;
     }
 
+    // This method selects the next node for the ant based on certain rules.
     selectNextNode() {
+        const currentNode = this.currentNode;
+        const unvisitedNeighbors = this.graph.neighbors(currentNode)
+            .filter(neighbor => !this.visited.has(neighbor))
+            .map(neighbor => ({
+                neighbor,
+                distance: this.graph.edge(currentNode, neighbor),
+                pheromone: this.pheromoneMatrix[currentNode][neighbor],
+            }));
 
+        if (unvisitedNeighbors.length === 0) {
+            return -1; // If other neighbors have been visited
+        }
+
+        let nextNode = -1;
+        if (Math.random() < this.params.q0) {
+            nextNode = this.selectNextNodeDeterministic(unvisitedNeighbors); // Let's choose by pheromones
+        } else {
+            nextNode = this.selectNextNodeProbabilistic(unvisitedNeighbors); // Choose next
+        }
+
+        return nextNode;
     }
 
     selectNextNodeDeterministic(neighborInfoList) {
