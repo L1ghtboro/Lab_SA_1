d7e7e1913411855ec762ea0087b8404b333ab6fd
L1ghtboro <tiptemka@gmail.com>
2023-09-30 21:31:40 +0200

    Ant System Final

--- Lab_SA_1/public/javascripts/min-max-ant-solution.js
+++ Lab_SA_1/public/javascripts/min-max-ant-solution.js
@@ -1,36 +1,89 @@
-const graphlib = require('graphlib');
-
-const graphModule = require('./graph-data'); 
-
-const graphRel = graphModule.createGraph();
-
-console.log(graphlib.json.write(graphRel));
-
 class MinMaxAntSystem {
-    constructor(graph, params) {
-
+    constructor(numNodes, params) {
+        this.numNodes = numNodes;
+        this.params = params || {
+            numAnts: 10,              // Number of ants
+            maxIterations: 100,      // Maximum iterations
+            alpha: 1.0,              // Influence of pheromone
+            beta: 2.0,               // Influence of distance
+            rho: 0.1,                // Pheromone evaporation rate
+            q0: 0.9,                 // Threshold for selecting the best next node
+            minPheromone: 0.01,      // Minimum pheromone level
+        };
+        this.pheromoneMatrix = this.initializePheromoneMatrix();
+        this.ants = [];
+        this.bestTour = null;
+        this.bestTourLength = Infinity;
     }
 
+    // Initialize the pheromone matrix with initial values
     initializePheromoneMatrix() {
+        const initialPheromone = 1.0 / (this.numNodes * this.params.rho);
+        const pheromoneMatrix = Array.from({ length: this.numNodes }, () =>
+            Array.from({ length: this.numNodes }, () => initialPheromone)
+        );
 
+        return pheromoneMatrix;
     }
 
+    updateBestTour() {
+        // Iterate through all the ants and find the ant with the shortest tour
+        for (const ant of this.ants) {
+            const tour = ant.tour;
+            if (tour.length > 0 && tour.length < this.bestTourLength) {
+                // Update the best tour and its length
+                this.bestTour = tour.slice(); // Clone the tour
+                this.bestTourLength = tour.length;
+            }
+        }
+    }
+
+    // Run the ant system algorithm
     run() {
-
+        for (let iteration = 0; iteration < this.params.maxIterations; iteration++) {
+            this.generateAnts();
+            this.updateBestTour();
+            this.updatePheromone();
+        }
+        return this.bestTour; // Return the best tour found
     }
 
+    // Generate ant solutions
     generateAnts() {
-
+        for (let i = 0; i < this.params.numAnts; i++) {
+            const ant = new Ant(this.numNodes, this.pheromoneMatrix, this.params);
+            const tour = ant.findTour();
+            if (tour.length > 0 && tour.length < this.bestTourLength) {
+                this.bestTour = tour; // Update the best tour if a shorter one is found
+                this.bestTourLength = tour.length;
+            }
+            this.ants.push(ant);
+        }
     }
 
+    // Update pheromone levels
     updatePheromone() {
+        for (let i = 0; i < this.numNodes; i++) {
+            for (let j = 0; j < this.numNodes; j++) {
+                this.pheromoneMatrix[i][j] *= (1 - this.params.rho);
+                if (this.pheromoneMatrix[i][j] < this.params.minPheromone) {
+                    this.pheromoneMatrix[i][j] = this.params.minPheromone;
+                }
+            }
+        }
 
+        for (let i = 0; i < this.bestTour.length - 1; i++) {
+            const from = this.bestTour[i];
+            const to = this.bestTour[i + 1];
+            this.pheromoneMatrix[from][to] += 1 / this.bestTourLength;
+            this.pheromoneMatrix[to][from] += 1 / this.bestTourLength;
+        }
     }
 }
 
 class Ant {
-    constructor(graph, pheromoneMatrix, params) {
-        this.graph = graph;
+    constructor(numNodes, pheromoneMatrix, params) {
+        this.numNodes = numNodes;
         this.pheromoneMatrix = pheromoneMatrix;
         this.params = params;
         this.visited = new Set();
@@ -41,7 +94,7 @@ class Ant {
 
     // This method is used to find the tour (path) for the ant.
     findTour() {
-        while (this.visited.size < this.graph.nodes().length) {
+        while (this.visited.size < this.numNodes) {
             const nextNode = this.selectNextNode();
             if (nextNode === -1) break;
             this.tour.push(nextNode);
@@ -54,45 +107,60 @@ class Ant {
     // This method selects the next node for the ant based on certain rules.
     selectNextNode() {
         const currentNode = this.currentNode;
-        const unvisitedNeighbors = this.graph.neighbors(currentNode)
+        const unvisitedNeighbors = Array.from({ length: this.numNodes }, (_, neighbor) => neighbor)
             .filter(neighbor => !this.visited.has(neighbor))
             .map(neighbor => ({
                 neighbor,
-                distance: this.graph.edge(currentNode, neighbor),
+                distance: this.params.distanceMatrix[currentNode][neighbor],
                 pheromone: this.pheromoneMatrix[currentNode][neighbor],
             }));
 
         if (unvisitedNeighbors.length === 0) {
-            return -1; // If other neighbors have been visited
+            return -1;
         }
 
         let nextNode = -1;
         if (Math.random() < this.params.q0) {
-            nextNode = this.selectNextNodeDeterministic(unvisitedNeighbors); // Let's choose by pheromones
+            nextNode = this.selectNextNodeDeterministic(unvisitedNeighbors);
         } else {
-            nextNode = this.selectNextNodeProbabilistic(unvisitedNeighbors); // Choose next
+            nextNode = this.selectNextNodeProbabilistic(unvisitedNeighbors);
         }
 
         return nextNode;
     }
 
+    // Select the next node deterministically
     selectNextNodeDeterministic(neighborInfoList) {
+        const probabilities = neighborInfoList.map(info => {
+            const p = Math.pow(info.pheromone, this.params.alpha) * Math.pow(1 / info.distance, this.params.beta);
+            return p;
+        });
 
+        const maxProbabilityIndex = probabilities.indexOf(Math.max(...probabilities));
+        return neighborInfoList[maxProbabilityIndex].neighbor;
     }
 
+    // Select the next node probabilistically
     selectNextNodeProbabilistic(neighborInfoList) {
+        const totalProbability = neighborInfoList.reduce((sum, info) => {
+            return sum + (Math.pow(info.pheromone, this.params.alpha) * Math.pow(1 / info.distance, this.params.beta));
+        }, 0);
 
+        const r = Math.random() * totalProbability;
+        let cumulativeProbability = 0;
+
+        for (const info of neighborInfoList) {
+            const p = (Math.pow(info.pheromone, this.params.alpha) * Math.pow(1 / info.distance, this.params.beta));
+            cumulativeProbability += p;
+            if (cumulativeProbability >= r) {
+                return info.neighbor;
+            }
+        }
+
+        return neighborInfoList[neighborInfoList.length - 1].neighbor;
     }
 }
-
-const graph = [
-    //Initlize graph here
-];
-
-const param = [
-    //Initilize params for MMSA
-];
 
 module.exports = {
-    MinMaxAntSystem
-}
+    MinMaxAntSystem,
+};
--- Lab_SA_1/routes/index.js
+++ Lab_SA_1/routes/index.js
@@ -4,6 +4,42 @@ var router = express.Router();
 
 var { MinMaxAntSystem } = require('../public/javascripts/min-max-ant-solution');
 
+// Define the distance matrix for the graph
+const distanceMatrix = [
+    [0, 8, 0, 0, 0, 0, 0, 8, 0, 0],
+    [8, 0, 9, 12, 0, 0, 0, 8, 0, 11],
+    [0, 9, 0, 0, 13, 15, 0, 0, 0, 0],
+    [0, 12, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 13, 0, 0, 15, 0, 0, 0, 0],
+    [0, 0, 15, 0, 15, 0, 22, 18, 0, 0],
+    [0, 0, 0, 0, 0, 22, 0, 0, 21, 0],
+    [8, 8, 0, 0, 0, 18, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 21, 0, 0, 12],
+    [0, 11, 0, 0, 0, 0, 0, 0, 12, 0],
+];
+
+// Define the parameters for the Min-Max Ant System
+const params = {
+    numAnts: 10,       // Number of ants
+    maxIterations: 100, // Maximum iterations
+    alpha: 1.0,        // Influence of pheromone
+    beta: 2.0,         // Influence of distance
+    rho: 0.1,          // Pheromone evaporation rate
+    q0: 0.9,           // Threshold for selecting the best next node
+    minPheromone: 0.01, // Minimum pheromone level
+    distanceMatrix: distanceMatrix, // Include the distance matrix
+    startNode: 8,     // Start from node 9
+    endNode: 4        // Go to node 5
+};
+
+// Create an instance of MinMaxAntSystem
+const antSystem = new MinMaxAntSystem(distanceMatrix.length, params);
+
+// Run the Min-Max Ant System
+const bestTour = antSystem.run();
+
+console.log('Best Tour:', bestTour);
+
 /* GET home page. */
 router.get('/', function (req, res) {
     res.render('index', { title: 'Express' });
